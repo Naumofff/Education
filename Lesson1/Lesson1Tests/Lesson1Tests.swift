c63c4581b3ac00f60c969ef840378e0309124522
Игорь Наумов <igornaumov@MacBookPro.dlink>
2025-04-26 09:23:56 +0300

    Lesson1

---
+++ Lesson1/Lesson1Tests/Lesson1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1Tests.swift
+//  Lesson1Tests
+//
+//  Created by Игорь Наумов on 26.04.2025.
+//
+
+import Testing
+@testable import Lesson1
+
+struct Lesson1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
